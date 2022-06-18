d4d02d7bcb3aa2ece901083374f664bb523a7951
Kumar Shivam Ray <kumarshivamray0656@gmail.com>
2022-06-19 05:01:21 +0530

    UPDATE

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ const App = (props) => {
                 <CardContent>
                   <Grid container direction={'column'} justifyContent="center" alignItems="center">
                     <Grid item>
-                      <Avatar alt={biodata.name} src={me} sx={{ width: 150, height: 150 }} />
+                      <Avatar alt={biodata.name} src={'https://shivam060897.github.io/portfolio/me.jpg'} sx={{ width: 150, height: 150 }} />
                     </Grid>
                     <Grid item>
                       <Typography variant='h6' align='center' color={'#FFFFFF'}>{biodata.name}</Typography>
