3e5f722b1adfd6aff2b31dc52815bfd80fde3b05
Kumar Shivam Ray <kumarshivamray0656@gmail.com>
2023-09-10 03:20:18 -0500

    Delete golive.cmd

--- golive.cmd
+++
@@ -1,1 +0,0 @@
-cls && RMDIR C:\Apache24\pages\portfolio /S /Q && RMDIR /Q /s docs && npm run build && ren build docs && XCOPY /S /I C:\Users\kumar\workspace\portfolio\docs\* C:\Apache24\pages\portfolio && git add . && git commit -m "UPDATE" && git push
